330d9df7930f9ed1e4199402b92b8f611d076e3a
aidyn <aidyn@MAZATRON>
2023-10-20 21:39:46 +0600

    Добавьте файлы проекта.

---
+++ Day7 (Tanks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MyClassLib.WordOfTanks;
+
+namespace Day7_Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] tanksT34 = new Tank[5];
+            Tank[] tanksPantera = new Tank[5];
+
+            for (int i = 0; i < 1; i++)
+            {
+                tanksT34[i] = new Tank("Т-34");
+                tanksPantera[i] = new Tank("Pantera");
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                bool t34Wins = tanksT34[i] ^ tanksPantera[i];
+                string winner = t34Wins ? "Т-34" : "Pantera";
+
+                Console.WriteLine($"Бой {i + 1}: {tanksT34[i].GetTankParameters()} vs {tanksPantera[i].GetTankParameters()}");
+                Console.WriteLine($"Победитель: {winner}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammunitionLevel;
+        private int armorLevel;
+        private int agilityLevel;
+
+        public Tank(string name)
+        {
+            Random random = new Random();
+            tankName = name;
+            ammunitionLevel = random.Next(0, 101);
+            armorLevel = random.Next(0, 101);
+            agilityLevel = random.Next(0, 101);
+        }
+
+        public string GetTankParameters()
+        {
+            return $"Tank Name: {tankName}, Ammunition: {ammunitionLevel}%, Armor: {armorLevel}%, Agility: {agilityLevel}%";
+        }
+
+        public static bool operator ^(Tank tank1, Tank tank2)
+        {
+            int winCriteriaCount = 0;
+
+            if (tank1.ammunitionLevel > tank2.ammunitionLevel)
+                winCriteriaCount++;
+            if (tank1.armorLevel > tank2.armorLevel)
+                winCriteriaCount++;
+            if (tank1.agilityLevel > tank2.agilityLevel)
+                winCriteriaCount++;
+
+            return winCriteriaCount >= 2;
+        }
+    }
+}
